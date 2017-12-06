52e6bc119c6bbcfafae4ca29e194e85111632156
Tommy <kbztommy@gmail.com>
2017-12-06 23:12:59 +0800

    base II

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_security import Security, SQLAlchemyUserDatastore
-
-
-db = SQLAlchemy()
-
+from .extensions import db, security
+from .main import main as main_blueprint
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -13,12 +11,13 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     db.init_app(app)
-    
+
     from .models import User, Role
     user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-    security = Security(app, user_datastore)
+    security.init_app(app, user_datastore)
 
-    from .main import main as main_blueprint
+    
     app.register_blueprint(main_blueprint)
 
     return app
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, current_app
 from flask_security import login_required
 from . import main
 from ..import db
@@ -10,3 +10,20 @@ from ..models import Summoner
 def index():
     summoner_list = Summoner.query.all()
     return render_template('index.html', summoner_list=summoner_list)
+
+
+@main.route('/get_summoner', methods=["GET", "POST"])
+@login_required
+def get_summoner():
+    current_app.logger.debug(request.form.get('name', 'form is empty'))
+    current_app.logger.debug(request.args.get('name', 'args is empty'))
+    current_app.logger.debug(request.values.get('name', 'values is empty'))
+    summoner = Summoner.query.filter_by(name=request.values['name']).first()
+    return "{} , {}".format(summoner.id, summoner.name)
+
+
+@main.route('/get_summoner_detail/<name>', methods=["GET", "POST"])
+@login_required
+def get_summoner_detail(name):
+    summoner = Summoner.query.filter_by(name=name).first()
+    return render_template('get_summoner_detail.html', summoner=summoner)
