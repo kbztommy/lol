1eeb902a3a8021ba164fdaed7478b994731af5e0
Tommy <kbztommy@gmail.com>
2018-01-08 17:12:21 +0800

    select champion modal

--- app/main/static_views.py
+++ app/main/static_views.py
@@ -1,7 +1,9 @@
+import json
 from flask import render_template, redirect, url_for
 from flask_security import login_required
 from . import main
 from ..service.static_service import update_item_data, update_champion_data, update_summoner_spell_data
+from ..filters import get_champion_id_map, champion_img_filter
 
 
 @main.route('/put_item_code', methods=['GET'])
@@ -23,3 +25,13 @@ def put_champion_code():
 def put_summoner_spell_code():
     update_summoner_spell_data()
     return redirect(url_for('main.index'))
+
+
+@main.route('/get_all_champion_img', methods=['GET', 'POST'])
+@login_required
+def get_all_champion_img():
+    champion_img_dict = dict()
+    champion_dict = get_champion_id_map()
+    for champion_id in champion_dict.keys():
+        champion_img_dict[champion_id] = champion_img_filter(champion_id)
+    return json.dumps(champion_img_dict)
